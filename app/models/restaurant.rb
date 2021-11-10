dcf8665db04b99b3075934793a64633b92673a6b
maximedecaritat <mdecaritat@gmail.com>
2021-11-10 12:46:17 +0100

    validation

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+
+    validates :name, presence: true
+    validates :category, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"]}
+   validates :address, presence: true
+    has_many :reviews, dependent: :destroy
+
+
+end
